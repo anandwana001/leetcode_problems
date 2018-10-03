7919babbe24fba75a3e08e4a82c0c205bbbbf324
Anshu Patel <patelanshu85@gmail.com>
2018-10-04 00:06:19 +0530

    Added Link and Problem Statement

--- algorithms/3SumsClosest.cpp
+++ algorithms/3SumsClosest.cpp
@@ -1,3 +1,13 @@
+#3Sum Closet
+//ProblemLink//https://leetcode.com/problems/3sum-closest/
+ 
+//Problem Statement: - Given an array nums of n integers and an integer target, find three integers in nums such that the sum is closest to target.
+//Return the sum of the three integers. You may assume that each input would have exactly one solution.
+
+//Example:
+//Given array nums = [-1, 2, 1, -4], and target = 1.
+//The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+
 // Time:  O(n^2)
 // Space: O(1)
 
