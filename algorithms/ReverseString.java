2b15d668c341423f8baa3145db263bcb2bf28127
SuchithKumar <kvsuchit@gmail.com>
2018-10-03 12:30:30 +0530

    Added Reverse String logic

---
+++ algorithms/ReverseString.java
@@ -0,0 +1,27 @@
+//https://leetcode.com/problems/reverse-string/
+
+//Problem
+
+//Write a function that takes a string as input and returns the string reversed.
+
+//Example 1:
+
+//Input: "hello"
+//Output: "olleh"
+
+//Example 2:
+
+//Input: "A man, a plan, a canal: Panama"
+//Output: "amanaP :lanac a ,nalp a ,nam A"
+
+
+//Solution
+
+
+
+class Solution {
+    public String reverseString(String s) {
+        StringBuilder rr = new StringBuilder(r);
+		return rr.reverse();        
+    }
+}
