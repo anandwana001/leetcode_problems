c28c67d7db02ecfae5117598250e90d4590b5a22
Junghoon Park <mugenjh@gmail.com>
2019-10-14 16:52:37 +0900

    Two Sum python
    Two Sum python

---
+++ algorithms/two_sum.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/two-sum/
+
+# Problem
+
+
+
+# Example:
+#
+# Given nums = [2, 7, 11, 15], target = 9,
+#
+# Because nums[0] + nums[1] = 2 + 7 = 9,
+# return [0, 1].
+
+
+
+# Solution
+
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(0, len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
